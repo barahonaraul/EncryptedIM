0bda6fb55f206007b26db79c95246360a5754fc4
reb <barahonaraul@live.com>
2016-09-21 00:18:41 -0400

    Gets encrypted but only 8 chars are unenrypted... no HMAC at all....

--- EncryptedIM.c
+++ EncryptedIM.c
@@ -9,13 +9,29 @@
 #include <netdb.h>
 #include <signal.h>
 #include <openssl/sha.h>
+#include <openssl/aes.h>
+#include <openssl/rand.h>
 
-
-#define PORTNO 9999
+#define PORTNO 9869
 #define BUFSIZE 1024
 
 int sock = -1;
 int conn_sock = -1;
+
+static void hex_print(const void* pv, size_t len)
+{
+    const unsigned char * p = (const unsigned char*)pv;
+    if (NULL == pv)
+        printf("NULL");
+    else
+    {
+        size_t i = 0;
+        for (; i<len;++i)
+            printf("%02X ", *p++);
+    }
+    printf("\n");
+}
+
 
 
 /*
@@ -61,7 +77,7 @@ char *parse_command_line( int argc, char *argv[] )
     }
 
     return NULL;		/* start in server mode */
-  } else if ((argc != 6) || (strcmp(argv[1],"-c") != 0) || (strcmp(argv[3],"-confkey") != 0) || (strcmp(argv[5],"-authkey") != 0)) {
+  } else if ((argc != 7) || (strcmp(argv[1],"-c") != 0) || (strcmp(argv[3],"-confkey") != 0) || (strcmp(argv[5],"-authkey") != 0)) {
     printf( "%s\n\n", usage ); 
     exit(1);
   }
@@ -149,6 +165,14 @@ int main( int argc, char *argv[] )
   int bytes_read;
   fd_set set;
   int nfds;
+  unsigned char IV[AES_BLOCK_SIZE];
+  unsigned char K1[16], K2[16];
+  unsigned char temp[SHA_DIGEST_LENGTH];
+
+      //Define a message pointer where all final data will be concatenated
+      unsigned char *message;
+  size_t encslength; //size of encryption buffer
+  //unsigned char HMAC[];
 	
 
   /* set up signal handler to deal with CTRL-C */
@@ -167,12 +191,21 @@ int main( int argc, char *argv[] )
   if (client == NULL) {
     /* if client is NULL, then we are the server, so wait for a new connection */
     conn_sock = wait_for_client(sock);
+
+   /* Get HASH values for K1 and K2 here because arg index dependant on -s|-c */
+   SHA1_16BYTES(K1, temp, argv[3]);
+   SHA1_16BYTES(K2, temp, argv[5]);
+
   } else {
     /* we are the client, so connect to the server */
     conn_sock = connect_to_client(sock,client);
+
+   /* Get HASH values for K1 and K2 here */
+   SHA1_16BYTES(K1, temp, argv[4]);
+   SHA1_16BYTES(K2, temp, argv[6]);
+
   }
 
-  /* Get HASH values for K1 and K2 here */
 
   
   while( 1 ) {
@@ -191,30 +224,61 @@ int main( int argc, char *argv[] )
     /* consider the two cases -- input from stdin or from the network */
     if (FD_ISSET(STDIN_FILENO,&set)) {
       /* process data from stdin */
-      bytes_read = read(STDIN_FILENO,buf,BUFSIZE);
-
+      bytes_read = read(STDIN_FILENO,buf,BUFSIZE);	
+	printf("bytes read: %d\n",bytes_read);
       if( bytes_read > 0){
 
       /* Generate IV here */
+	memset(IV,0x0,AES_BLOCK_SIZE);
 
+	
+	RAND_bytes(IV, AES_BLOCK_SIZE); // IV now holds our init vector of 16 bytes
+ 
       /* Generate HMAC here using K2 and data read */
 
       /* Concat HMAC with data read */
 
-      /* Encrypt HMAC + data read Here */
+      /* Encrypt HMAC + data read Here */ 
+	//Lets see if we can encrypt for now...
+    
+    // buffers for encryption
+    encslength = ((bytes_read + AES_BLOCK_SIZE) / AES_BLOCK_SIZE) * AES_BLOCK_SIZE;
+    unsigned char enc_out[encslength];
+    memset(enc_out, 0, sizeof(enc_out));
+
+    message = (unsigned char*) malloc(AES_BLOCK_SIZE + encslength);
+    printf("size of msg1:%ld\n",sizeof(message));
+    memcpy(message,IV,16);
+
+    // Set up the key and encrypt
+    AES_KEY enc_key;
+    AES_set_encrypt_key(K1, 128, &enc_key);
+
+    AES_cbc_encrypt(buf, enc_out, bytes_read, &enc_key, IV, AES_ENCRYPT);
+
+	
+
+	//hex_print(message,encslength+16);
+	memcpy(message+AES_BLOCK_SIZE,enc_out,encslength);
+	
+	//hex_print(message,encslength+16);
 
       /* Concat IV and Encrypted Data Here and get size of whole */
       
-
+	
 
 
       }
-
-      write(conn_sock,buf,bytes_read); /* echo it back CHANGE BUFFER AND BYTES READ*/
+      printf("size of msg2:%ld\n",sizeof(message));
+      write(conn_sock,message,AES_BLOCK_SIZE + encslength); /* echo it back CHANGE BUFFER AND BYTES READ*/
+      free(message);
     }
     if (FD_ISSET(conn_sock,&set)) {
       /* process data from the connection */
       bytes_read = read(conn_sock,buf,BUFSIZE);
+      printf("bytes read rec: %d\n",bytes_read);
+      unsigned char *dec_out;
+
       if (bytes_read == 0) {
 	/* end-of-file! */
 	break;
@@ -223,11 +287,29 @@ int main( int argc, char *argv[] )
 
       /*Get IV (first 16 bytes) from received data */
 
+      memset(IV,0x0,AES_BLOCK_SIZE);
+      memcpy(IV, &buf[0], AES_BLOCK_SIZE);
+	//hex_print(buf,BUFSIZE);
+
+      unsigned char to_dec[bytes_read - AES_BLOCK_SIZE];
+      memset(to_dec, 0, sizeof(to_dec));
+      printf("br-16:%d\n",bytes_read - AES_BLOCK_SIZE);
+      memcpy(to_dec, &buf[AES_BLOCK_SIZE], bytes_read - AES_BLOCK_SIZE);
+      //hex_print(to_dec,bytes_read - AES_BLOCK_SIZE);
+
+      dec_out = (unsigned char *) malloc(bytes_read - AES_BLOCK_SIZE);
+      memset(dec_out, 0, sizeof(dec_out));
+      
+      AES_KEY dec_key;
+      AES_set_decrypt_key(K1, 128, &dec_key);
+      AES_cbc_encrypt(to_dec, dec_out, bytes_read - AES_BLOCK_SIZE, &dec_key, IV, AES_DECRYPT);
+
       /*Using IV decrypt rest of the message */
 
       /* Seperate Decrypted code to get HMAC and decrypted data */
 
-      /* Generate HMAC using K2 and compare to decrypted value if good HMAC then do nohing (if bad HMAC msg and quit)*/
+      /* Regenerate HMAC using K2 and decrypted data and compare to decrypted HMAC, if good HMAC then do nohing 
+	(if bad HMAC msg and quit)*/
 
       
       /* set data to be printed in a buffer */
@@ -236,7 +318,8 @@ int main( int argc, char *argv[] )
 
 
       }
-      write(STDOUT_FILENO,buf,bytes_read); /* write received data to stdout CHANGE BUFFER AND BYTES READ*/
+      write(STDOUT_FILENO,dec_out,sizeof(dec_out)); /* write received data to stdout CHANGE BUFFER AND BYTES READ*/
+      free(dec_out);
     }
   }
 
